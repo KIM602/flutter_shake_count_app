9feda9cf9957a2efa25d2997576c4255dd7ad881
KIM602 <kyk940608@gmail.com>
2023-10-11 16:36:39 +0900

    flutter native screen 추가

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -6,15 +6,6 @@ import 'package:velocity_x/velocity_x.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -76,7 +67,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 const RedBox(),
                 Column(
                   children: [
-                    RedBox().box.padding(EdgeInsets.all(30)).color(Colors.blue).make(),
+                    RedBox()
+                        .box
+                        .padding(EdgeInsets.all(30))
+                        .color(Colors.blue)
+                        .make(),
                     '흔들어서 카운트를 올려보세요.'
                         .text
                         .color(Colors.red)
